7c9bb349173538d1f8f2164c815854cff1afa95b
Travis Collins <travis.collins@analog.com>
2019-02-08 09:25:37 -0800

    libiio updates
    Signed-off-by: Travis Collins <travis.collins@analog.com>

--- libiio.rb
+++ libiio.rb
@@ -2,17 +2,19 @@
 class Libiio < Formula
   desc "libIIO"
   homepage "https://github.com/analogdevicesinc/iio-oscilloscope/wiki"
-  head "https://github.com/analogdevicesinc/libiio.git"
+#  head "https://github.com/analogdevicesinc/libiio.git"
+  url "https://github.com/analogdevicesinc/libiio/archive/v0.17.tar.gz"
 
   # libusb libxml2
   depends_on "cmake" => :build
-  depends_on "gtk+" => :build
-  depends_on "glib" => :build
+#  depends_on "gtk+" => :build
+#  depends_on "glib" => :build
+#  depends_on "libcdk5-dev" => :build
+#  depends_on "bison" => :build
+#  depends_on "flex" => :build
+  depends_on "doxygen" => build
   depends_on "libxml2" => :build
   depends_on "libusb" => :build
-  depends_on "bison" => :build
-  depends_on "flex" => :build
-#  depends_on "libcdk5-dev" => :build
 
   def install
     ENV.deparallelize  # if your formula fails when building in parallel
